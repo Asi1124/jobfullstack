51645d69e34836e0ebe4486ad9aeca25b3f0bad3
Asi <149960376+Asi1124@users.noreply.github.com>
2024-05-27 17:49:37 +0300

    fullstack test

---
+++ database/migrations/2024_05_27_135139_create_publications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->json('photo_urls');
+            $table->decimal('price',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
